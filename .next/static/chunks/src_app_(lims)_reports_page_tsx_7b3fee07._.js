6a06c3d7b79b4d69fa486d461369a94ba3d4056e
Amabio <150064661+Amabio@users.noreply.github.com>
2025-07-26 13:32:07 +0100

    Add .next/static/chunks/src_app_(lims)_reports_page_tsx_7b3fee07._.js 1753533127534

---
+++ .next/static/chunks/src_app_(lims)_reports_page_tsx_7b3fee07._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f61de0f5._.js",
+  "static/chunks/node_modules_zod_9fcf6f3a._.js",
+  "static/chunks/node_modules_lodash_104e19b0._.js",
+  "static/chunks/node_modules_recharts_es6_97b27b7a._.js",
+  "static/chunks/node_modules_@radix-ui_a98a625e._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_93ffbbe1._.js"
+],
+    source: "dynamic"
+});
