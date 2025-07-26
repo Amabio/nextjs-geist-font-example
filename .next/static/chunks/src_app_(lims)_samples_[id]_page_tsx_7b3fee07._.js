456f75ae82ad9e98aeb0d08d62ec6451a45fed32
Amabio <150064661+Amabio@users.noreply.github.com>
2025-07-26 13:32:06 +0100

    Add .next/static/chunks/src_app_(lims)_samples_[id]_page_tsx_7b3fee07._.js 1753533126648

---
+++ .next/static/chunks/src_app_(lims)_samples_[id]_page_tsx_7b3fee07._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a4b57619._.js",
+  "static/chunks/node_modules_@radix-ui_49bf0a82._.js"
+],
+    source: "dynamic"
+});
